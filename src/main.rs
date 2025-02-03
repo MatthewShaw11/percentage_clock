011025ac7d57e606aa24ea6fbb7334b131af8ac7
Matt Shaw <matt@shaw.pw>
2025-02-03 11:55:14 -0500

    it just works trade mark

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
+use std::{thread::sleep, time::Duration};
+
 mod percentage_clock;
 
 fn main() {
     
+    let mut last_time = String::new();
     loop {
-        let dream = percentage_clock::get_time();
-        println!("{dream}");
+        let time = percentage_clock::get_time();
+        if last_time == time {
+            sleep(Duration::new(0,250_000_000));
+            continue;
+        }
+
+        println!("{time}");  
+        last_time = time;  
+    
     }
     
 }
