6e3776e8c94c6ed147e51045cba4f0bb37a1dc81
doke <doke@Makmake>
2025-02-20 22:41:17 -0500

    modifed so that the --clock behavior is not the default

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,38 @@
-use std::{thread::sleep, time::Duration};
+use std::{
+    thread::sleep, 
+    time::Duration, 
+    env
+};
 
 mod percentage_clock;
 
 fn main() {
     
+    let args: Vec<String> = env::args().collect();
+    
+    if args.iter().count() > 1 {
+        let first_opt = args
+            .get(1)
+            .unwrap()
+            .trim()
+            .to_ascii_lowercase();
+
+        if first_opt == "--clock"
+        {
+            clock();
+        }
+        else {
+            println!("The only valid option you can provide is \"--clock\" to make this run as a continuously running clock synced with your computers time.");
+        }
+    }
+    else {
+        let time = percentage_clock::get_time();
+        println!("{time}");  
+    }
+}
+
+
+fn clock() {
     let mut last_time = String::new();
     loop {
         let time = percentage_clock::get_time();
@@ -14,8 +43,5 @@ fn main() {
 
         println!("{time}");  
         last_time = time;  
-    
     }
-    
 }
-
